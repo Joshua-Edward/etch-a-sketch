90efe965853b94b78e4e95d8294debcd0b6271fe
Joshua-Edward <109255071+Joshua-Edward@users.noreply.github.com>
2023-02-25 15:50:54 -0800

    Make grid reseet when size is updated

--- javascript.js
+++ javascript.js
@@ -6,23 +6,38 @@ console.log(gridSize);
 
 gridSize.oninput = function() {
   output.innerHTML = this.value;
+  removeAllChildNodes(divGridContainer);
+  gridGenerate();
 }
 
 let divGridContainer = document.getElementById('gridContainer');
 
-for (j = 0; j < gridSize.value; j++) {
-  const gridRow = document.createElement('div');
-  gridRow.classList.add('rowElement');
-    for (i = 0; i < gridSize.value; i++) {
-      const gridElement = document.createElement('div');
-      gridElement.classList.add('pixelElement');
-      gridElement.addEventListener('mouseover', changeColor);
-      gridRow.appendChild(gridElement);
-    }
-    divGridContainer.appendChild(gridRow);
+// Makes a grid per the gridSize selected on the slider
+function gridGenerate() {
+  for (j = 0; j < gridSize.value; j++) {
+    const gridRow = document.createElement('div');
+    gridRow.classList.add('rowElement');
+      for (i = 0; i < gridSize.value; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('pixelElement');
+        gridElement.addEventListener('mouseover', changeColor);
+        gridRow.appendChild(gridElement);
+      }
+      divGridContainer.appendChild(gridRow);
+  }
 }
+
+// Removes existing pixel divs when generating a new size
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+gridGenerate();
+
+
 
 function changeColor(e) {
   e.target.style.backgroundColor = '#f4f4f4';
 }
-
