b906014a2f0afaca97b3eabf61cde0411f14d795
Joshua-Edward <109255071+Joshua-Edward@users.noreply.github.com>
2023-02-25 15:09:00 -0800

    Add slider bar for selecting grid size

--- javascript.js
+++ javascript.js
@@ -1,10 +1,19 @@
-let gridSize = 64;
+let gridSize = document.getElementById('myRange');
+let output = document.getElementById('sizeDisplay')
+output.innerHTML = gridSize.value;
+
+console.log(gridSize);
+
+gridSize.oninput = function() {
+  output.innerHTML = this.value;
+}
+
 let divGridContainer = document.getElementById('gridContainer');
 
-for (j = 0; j < gridSize; j++) {
+for (j = 0; j < gridSize.value; j++) {
   const gridRow = document.createElement('div');
   gridRow.classList.add('rowElement');
-    for (i = 0; i < gridSize; i++) {
+    for (i = 0; i < gridSize.value; i++) {
       const gridElement = document.createElement('div');
       gridElement.classList.add('pixelElement');
       gridElement.addEventListener('mouseover', changeColor);
@@ -16,3 +25,4 @@ for (j = 0; j < gridSize; j++) {
 function changeColor(e) {
   e.target.style.backgroundColor = '#f4f4f4';
 }
+
